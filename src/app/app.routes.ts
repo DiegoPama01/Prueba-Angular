1cc24586fc3f9450a05fc7b985ab7e34fe64de60
Diego <diego2001pama@gmail.com>
2026-02-03 15:41:32 +0100

    feat: Refactor routing to use loadComponent for lazy loading of components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,14 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './components/login-component/login-component';
-import { DashboardComponent } from './components/dashboard-component/dashboard-component';
-import { ProjectsComponent } from './components/dashboard-component/pages/projects-component/projects-component';
-import { StatisticsComponent } from './components/dashboard-component/pages/statistics-component/statistics-component';
 
 export const routes: Routes = [
-	{ path: '', component: LoginComponent },
-	{
-		path: 'dashboard',
-		component: DashboardComponent,
-		children: [
-			{ path: '', redirectTo: 'projects', pathMatch: 'full' },
-			{ path: 'projects', component: ProjectsComponent },
-			{ path: 'statistics', component: StatisticsComponent },
-		]
-	}
+  { path: '', loadComponent: () => import('./components/login-component/login-component').then(m => m.LoginComponent) },
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./components/dashboard-component/dashboard-component').then(m => m.DashboardComponent),
+    children: [
+      { path: '', redirectTo: 'projects', pathMatch: 'full' },
+      { path: 'projects', loadComponent: () => import('./components/dashboard-component/pages/projects-component/projects-component').then(m => m.ProjectsComponent) },
+      { path: 'statistics', loadComponent: () => import('./components/dashboard-component/pages/statistics-component/statistics-component').then(m => m.StatisticsComponent) },
+    ]
+  }
 ];
